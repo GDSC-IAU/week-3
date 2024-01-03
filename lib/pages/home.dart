e68c56e519f7c4776c7eace2df2e618e16397671
ferhad_Baaqieel <ferhad.baaqieel@gmail.com>
2024-01-03 09:06:53 +0300

    app bar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,7 +8,18 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('my app'),
+        title: const Text(
+          'Breakfast',
+          style: TextStyle(
+              color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        leading: Container(
+          margin: EdgeInsets.all(10),
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+          child: SvgPicture.asset('assets/icons/Arrow - Left 2.svg'),
+        ),
       ),
     );
   }
